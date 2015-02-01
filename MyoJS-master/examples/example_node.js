f8105c0e54b7cc64510437b4ca626e71de3897da
Newton Jain <newton.jain@gmail.com>
2015-02-01 01:00:25 -0500

    improved file system

--- MyoJS-master/examples/example_node.js
+++ MyoJS-master/examples/example_node.js
@@ -1,26 +1,66 @@
+var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+
+var outputString;
+color = 'FFFFFF';
+Fist = false;
+
+//console.log(outputString);
+btSerial.on('found', function(address, name) {
+    btSerial.findSerialPortChannel(address, function(channel) {
+        btSerial.connect(address, channel, function() {
+            console.log('connected');
+            outputString = 'Hi';
+            // var string='';
+            // var mecolor ='FFFFFF';
+
+
+                   setInterval(function() {
+
+       // if(string!=outputString || mecolor!=color){
+            // string = outputString;
+            // mecolor = color;
+
+            btSerial.write(new Buffer('b' + color + outputString, 'utf-8'), function(err, bytesWritten) {
+                    if (err) console.log(err);
+                });
+           //  }
+            console.log(outputString);
+            console.log(color);
+        }, 7000);
+
+   
+
+            btSerial.on('data', function(buffer) {
+                console.log(buffer.toString('utf-8'));
+            });
+        }, function () {
+            console.log('cannot connect');
+        });
+
+        // close the connection when you're ready
+      //  btSerial.close();
+    }, function() {
+        console.log('found nothing');
+    });
+});
+
+btSerial.inquire();
+
 //global varibles
-
-//sentences that are temp
-simpleSentences = function (){
-
-  var sentenceArr = ["Hi", "Bye", "How are you doing?", "What are your plans for today?", "What's up?"]
-  
-  return sentenceArr;
-};
-
 
 //first index is counter for actions, second and third index has to do with the interval between actions
 tempArray={
-	"Hi":[0,0,0], //wave in, wave out: "Hi"
-	"Bye":[0,0,0], //wave out: "Bye"
-	"How":[0,0,0], //fist, fingers spread: "How are you doing?"
-	"What":[0,0,0], //wave in, fist: "What are your plans for today?"
-	"Whats":[0,0,0], //wave out, fist: "What's up?" 
-	"rest":0
+	"Hi":[0,0], //wave in, wave out: "Hi"
+	"Bye":[0,0], //wave out: "Bye"
+	"How":[0,0], //wave out, wave in: "How are you doing?"
+	"What":[0,0], //wave in, fist: "What are your plans for the weekend?"
+	"Ilu":[0,0], //wave out, fist: "I Love you?" 
+	"rest":0,
+	"fistRest":0
 };
 
-Fist = false;
-color = 'FFFFFF';
+outputString = "";
+
 
 //myo connection
 var Myo = require('../template/entry'),
@@ -45,70 +85,177 @@ hub.on('pose', function(pose) {
     currentPose = pose;
     
     switch(currentPose.type) {
+    	//Fist
         case currentPose.POSE_FIST:
         Fist = true;
+        tempArray.fistRest = tempArray.rest;
+            console.log("fist");
 
-            console.log("i am making a fist");
-            if(tempArray.Hi[0] == 0){
-            	tempArray.Hi[0] = tempArray.Hi[0] + 1	
+            /*
+            "What"
+            */
+            //console.log(tempArray.rest)
+           // console.log(tempArray.What[1])
+            if(tempArray.What[0] == 1 && (tempArray.rest - tempArray.What[1] <= 100)){
+            	console.log("What are you plans for the weekend?");
+            	outputString = "What are you plans for the weekend?"
+       			tempArray.What[0] = 0;
+       			tempArray.What[1] = 0;
             }
-            if(tempArray.How[0] == 0){
-            	tempArray.How[0] = tempArray.How[0] + 1	
+
+            /*
+            "Ilu"
+            */
+            
+            if(tempArray.Ilu[0] == 1 && (tempArray.rest - tempArray.Ilu[1] <= 100)){
+            	console.log("I love you");
+            	outputString = "I love you"
+       			tempArray.Ilu[0] = 0;
+       			tempArray.Ilu[1] = 0;
             }
             break;
+
+        //Wave in
         case currentPose.POSE_WAVE_IN:
-        Fist = false;
-            console.log("i am making a wave in");
+            console.log("wave in");
+
+            /*
+			"Hi"
+			*/
             if(tempArray.Hi[0] == 0){
             	tempArray.Hi[0] = tempArray.Hi[0] + 1
             	tempArray.Hi[1] = tempArray.rest	
-            	//console.log(tempArray.Hi[0]);
             }
-            break;
-        case currentPose.POSE_WAVE_OUT:
-        Fist = false;
-        	tempArray.Hi[2] = tempArray.rest;
-        	//console.log("tempArray.Hi[2] = " + tempArray.Hi[2] + " tempArray.Hi[1] = " + tempArray.Hi[1]);
-        	//console.log(tempArray.Hi[2] - tempArray.Hi[1]);
-            console.log("i am making a wave out");
-            if(tempArray.Hi[0] == 0){
-            	console.log("Bye");
+            else{
+            	tempArray.Hi[1] = tempArray.rest
             }
-            if(tempArray.Hi[0] == 1 && (tempArray.Hi[2] - tempArray.Hi[1] <= 35)){
-            	console.log("Hi");
-       			tempArray.Hi[0] = 0;
-       			console.log(tempArray.Hi[0]);
+
+
+            /*
+			"What"
+			*/
+            if(tempArray.What[0] == 0){
+            	tempArray.What[0] = tempArray.What[0] + 1
+            	tempArray.What[1] = tempArray.rest	
             }
-            break;
-        case currentPose.POSE_FINGERS_SPREAD:
-        Fist = false;
-           	console.log("i am making a fingers spreading motion");
-           	if(tempArray.How[0] == 1){
+            else{
+            	tempArray.What[1] = tempArray.rest
+            }
+
+            /*
+           	"How"
+           	*/
+           	//console.log("tempArray.rest = " + tempArray.rest)
+           	//console.log("tempArray.How[1] = " + tempArray.How[1])
+           	if(tempArray.How[0] == 1 && (tempArray.rest - tempArray.How[1] <= 100)){
             	console.log("How are you doing?");
+            	outputString = "How are you doing?"
             	tempArray.How[0] = 0;
+            	tempArray.How[1] = 0;
             }
+
+
+
+            break;
+
+        //Wave out
+        case currentPose.POSE_WAVE_OUT:
+  			console.log("wave out");
+
+  			/*
+			"Bye"
+  			*/
+            console.log("Bye");
+            outputString = "Bye"
+  			
+
+            /*
+        	"Hi"
+        	*/
+            if(tempArray.Hi[0] == 1 && (tempArray.rest - tempArray.Hi[1] <= 100)){
+            	console.log("Hi");
+            	outputString = "Hi"
+       			tempArray.Hi[0] = 0;
+            }
+
+            /*
+            "Ilu"
+            */
+            if(tempArray.Ilu[0] == 0){
+            	tempArray.Ilu[0] = tempArray.Ilu[0] + 1
+
+            	tempArray.Ilu[1] = tempArray.rest
+            	console.log(tempArray.Ilu[1])
+            }
+            else{
+            	tempArray.Ilu[1] = tempArray.rest
+            	console.log(tempArray.Ilu[1])
+            }
+
+            /*
+			"How"
+            */
+            if(tempArray.How[0] == 0){
+            	tempArray.How[0] = tempArray.How[0] + 1
+            	tempArray.How[1] = tempArray.rest;
+            }
+            else{
+            	tempArray.How[1] = tempArray.rest;
+            }
+
+            
+
+
+            break;
+
+        //Finger spread
+        case currentPose.POSE_FINGERS_SPREAD:
+           	console.log("fingers spreading");
+
+           	
            
             break;
+
+        //Twist in
         case currentPose.POSE_TWIST_IN:
-        Fist = false;
-            console.log("i am making a twist in");
+            console.log("twist in");
             break;
+
+
+        //Nothing
         case currentPose.POSE_NONE:
         default:
 	        //console.log("rest")
 	        tempArray.rest = tempArray.rest + 1;
-	        if(tempArray.Hi[1] < (tempArray.rest - 35)){
+            if(tempArray.fistRest < (tempArray.rest - 100)){
+                Fist = false;
+                //console.log("Fist is false")
+            }
+	        /*
+	        Check times in all sentence objects and if later than a 35 count, reduce them to zero
+	        */
+           
+	        if(tempArray.Hi[1] < (tempArray.rest - 100)){
 	        	tempArray.Hi[0] = 0;
 	        }
-	        //console.log(tempArray.Hi);
+	        if(tempArray.How[1] < (tempArray.rest - 100)){
+	        	tempArray.How[1] = 0;
+	        }
+			if(tempArray.What[1] < (tempArray.rest - 100)){
+	        	tempArray.What[1] = 0;
+	        }
+	        if(tempArray.Ilu[1] < (tempArray.rest - 100)){
+	        	tempArray.Ilu[1] = 0;
+	        }		     
             break;
     }
 
 });
+ //////////////////////////color///////////
 
 hub.on('frame', function(frame) {
     
-    // if(Fist){
+     if(Fist){
   //  console.dir(frame);
      
     if (frame.rotation ) {
@@ -134,7 +281,7 @@ hub.on('frame', function(frame) {
             console.log("color is  still while, WHITE, try something new");
         }
 
-    // }
+     }
 
 
     }
@@ -143,47 +290,3 @@ hub.on('frame', function(frame) {
       //  console.log(frame.pose.toString());
     }
 });
-
-
-var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-
-btSerial.on('found', function(address, name) {
-    btSerial.findSerialPortChannel(address, function(channel) {
-        btSerial.connect(address, channel, function() {
-            console.log('connected');
-
-            var feed;
-            var string='a';
-
-                   setInterval(function() {
-
-    if(string!=feed){
-        feed= string;
-
-     btSerial.write(new Buffer('bFFFFFF'+ feed, 'utf-8'), function(err, bytesWritten) {
-                if (err) console.log(err);
-            });
- }
-
-     console.log(color);
-
-
-}, 8000);
-
-   
-
-            btSerial.on('data', function(buffer) {
-                console.log(buffer.toString('utf-8'));
-            });
-        }, function () {
-            console.log('cannot connect');
-        });
-
-        // close the connection when you're ready
-      //  btSerial.close();
-    }, function() {
-        console.log('found nothing');
-    });
-});
-
-btSerial.inquire();
